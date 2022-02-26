e9b884a64502db50fc3662a3b08f2af41154c8da
sumeet-toptal <sumeet@toptal.com>
2022-02-26 18:56:49 +0530

    Add Error handling: WIP4

---
+++ _SQLFiles/14_file_checker_4.sql
@@ -0,0 +1,39 @@
+use Alegeus_File_Processing;
+go
+
+create function fileLogHasError( @fileLogId int ) returns int
+begin
+    declare @cnt int = 0;
+    select
+        @cnt = count( * )
+    from
+        dbo.file_processing_tasks_log
+    where
+          fileLogId = @fileLogId
+      and processingTaskOutcome = 'ERROR';
+    
+    return isnull( @cnt , 0 );
+end
+GO
+alter view vw_file_processing_log
+as
+    select
+        fileLogId
+      , platform
+      , fileId
+      , folderName
+      , templateType
+      , ICType
+      , toFTP
+      , bencode
+      , originalFileName
+      , originalFullPath
+      , newFileName
+      , newFileFullPath
+      , created_at
+      , originalFileUploadedOn
+      , dbo.fileLogHasError( fileLogId ) ProcessingErrorCount
+    from
+        file_processing_log h;
+
+select * from vw_file_processing_log where ErrorCount > 0;
