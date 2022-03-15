34a80c14d9d3f12b01574030c4a278b45167aef8
sumeet-toptal <sumeet@toptal.com>
2022-03-15 21:06:07 +0530

    New Database Data_Processing

--- __LocalTestDirsAndFiles/copy_Alegeus_mbi+res_to_export_ftp.bat
+++ __LocalTestDirsAndFiles/copy_Alegeus_mbi+res_to_export_ftp.bat
@@ -1,2 +1,4 @@
-del %~dp0\FTP\AlegeusSourceFiles_New\*.* /q 
-xcopy %~dp0AlegeusResultFiles\*.* %~dp0_local_FTP_Server_Server\Alegeus\ /y
+del %~dp0_local_FTP_Server_Server\Alegeus\*.* /q 
+
+xcopy %~dp0AlegeusResultFiles\mbi\*.* %~dp0_local_FTP_Server_Server\Alegeus\ /y
+xcopy %~dp0AlegeusResultFiles\res\*.* %~dp0_local_FTP_Server_Server\Alegeus\ /y
--- __LocalTestDirsAndFiles/copy_Alegeus_source_files_to_import_ftp.bat
+++ __LocalTestDirsAndFiles/copy_Alegeus_source_files_to_import_ftp.bat
@@ -1,9 +1,4 @@
-del %~dp0FTP\AlegeusSourceFiles_New\*.* /q 
-del %~dp0FTP\AlegeusSourceFiles_Old\*.* /q  
-del %~dp0FTP\AlegeusSourceFiles_NoChange\*.* /q 
-del %~dp0FTP\AlegeusSourceFiles_Own\*.* /q 
+del %~dp0FTP\AlegeusSourceFiles\*.* /q 
 
-xcopy %~dp0AlegeusSourceFiles_New\*.* %~dp0FTP\AlegeusSourceFiles_New\ /y
-xcopy %~dp0AlegeusSourceFiles_Old\*.* %~dp0FTP\AlegeusSourceFiles_Old\ /y
-xcopy %~dp0AlegeusSourceFiles_NoChange\*.* %~dp0FTP\AlegeusSourceFiles_NoChange\ /y
-xcopy %~dp0AlegeusSourceFiles_Own\*.* %~dp0FTP\AlegeusSourceFiles_Own\ /y
+
+xcopy %~dp0AlegeusSourceFiles\*.* %~dp0FTP\AlegeusSourceFiles\ /y
