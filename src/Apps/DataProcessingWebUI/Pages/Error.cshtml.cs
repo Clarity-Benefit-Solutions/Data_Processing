b4b8d61589dcfb83c3945e9755e3a70e0b8b1d56
sumeet-toptal <sumeet@toptal.com>
2022-04-06 15:24:14 +0530

    upgrade to VS 2022 and .Net Core v6

--- src/Apps/DataProcessingWebUI/Pages/Error.cshtml.cs
+++ src/Apps/DataProcessingWebUI/Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace DataProcessingWebUIv6.Pages
+namespace DataProcessingWebUI.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
