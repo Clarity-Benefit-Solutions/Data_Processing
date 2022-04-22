89e5bbb47386579206eec91f0a4b6f611f4c57e4
sumeet-toptal <sumeet@toptal.com>
2022-04-22 12:23:07 +0530

    reverted webUI code cleaup

--- src/Apps/DataProcessingWebUI/Pages/Error.cshtml.cs
+++ src/Apps/DataProcessingWebUI/Pages/Error.cshtml.cs
@@ -1,28 +1,27 @@
-﻿namespace DataProcessingWebUI.Pages
-{
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
+namespace DataProcessingWebUI.Pages
+{
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
     public class ErrorModel : PageModel
     {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
         private readonly ILogger<ErrorModel> _logger;
 
         public ErrorModel(ILogger<ErrorModel> logger)
         {
-            this._logger = logger;
-        }
-
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId
-        {
-            get { return !string.IsNullOrEmpty(this.RequestId); }
+            _logger = logger;
         }
 
         public void OnGet()
         {
-            this.RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
         }
     }
-
 }
