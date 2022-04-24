fc64a2b0857d1e6337da2beb92dee67cb38a05b7
sumeet-toptal <sumeet@toptal.com>
2022-04-24 13:53:43 +0530

    copy test files

--- __LocalTestDirsAndFiles/copy_Alegeus_source_files_to_import_ftp.bat
+++
@@ -1,6 +0,0 @@
-del %~dp0FTP\_AlegeusSourceFiles\*.* /q 
-
-rmdir %~dp0FTP\_AlegeusSourceFiles\ /Q /S
-mkdir %~dp0FTP\_AlegeusSourceFiles\
-
-xcopy %~dp0AlegeusSourceFiles\*.* %~dp0FTP\_AlegeusSourceFiles\ /y
